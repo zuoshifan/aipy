6ad7d62f9dbbd5e03c9d58101a3ea239011e8ce2
David Moore <damo@sas.upenn.edu>
2012-03-05 15:13:11 -0500

    gave useful labels to deconv1d_test.py

--- tests/deconv1d_test.py
+++ tests/deconv1d_test.py
@@ -12,16 +12,28 @@ print 'cln', cln
 print info
 print '-----------------------------------------------------------------'
 
+p.subplot(311)
+p.title('Real')
+p.plot(n.abs(cln), 'b')
+p.plot(n.abs(img), 'k.')
+p.plot(n.abs(img - cln), 'r')
+
 img = n.array([0,0,0,4j,6j,4j,0,0,2+2j,3+3j,2+2j,0], dtype=n.complex)
 ker = n.array([3,2,0,0,0,0,0,0,0,0,0,2], dtype=n.complex)
 
 print 'CMPLX TEST:'
 print 'img', img
 print 'ker', ker
-mdl, info = a.deconv.clean(img, ker)
+cln, info = a.deconv.clean(img, ker)
 print 'cln', cln
 print info
 print '-----------------------------------------------------------------'
+p.subplot(312)
+p.title('Complex, real part')
+p.plot(cln.real, 'b',label='Model')
+p.plot(img.real, 'k.',label='Image')
+p.plot((img - cln).real, 'r',label='Resid')
+p.legend()
 
 SIZE = 16
 img = n.zeros((SIZE,), n.complex)
@@ -39,7 +51,9 @@ cln, info = a.deconv.clean(d1d, ker, maxiter=200)
 print 'cln', cln
 print info
 
-p.plot(n.abs(cln), 'b')
-p.plot(n.abs(img), 'k.')
-p.plot(n.abs(img - cln), 'r')
+p.subplot(313)
+p.title('Complex, imaginary part')
+p.plot(cln.imag, 'b')
+p.plot(img.imag, 'k.')
+p.plot((img - cln).imag, 'r')
 p.show()
